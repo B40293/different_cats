a97c43a31889181dd594f86e6a71e4cc98f88743
Kira Bystrova <kira.bystrova@gmail.com>
2024-11-08 08:43:24 +0300

    Добавлено изменение отображения картинок

--- cats2.py
+++ cats2.py
@@ -10,6 +10,7 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Произщшла ошибка: {e}")
@@ -26,9 +27,9 @@ def set_image():
 window = Tk()
 
 window.title("Cats!")
-window.geometry('600x480')
+window.geometry('600x520')
 
-update_button=Button(text="",command=set_image)
+update_button=Button(text="Обновить",command=set_image)
 update_button.pack()
 
 label = Label()
