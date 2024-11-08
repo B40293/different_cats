389c9f1b4343e189abdbdba19d1f28c7084bd8ea
Kira Bystrova <kira.bystrova@gmail.com>
2024-11-08 11:15:46 +0300

    Новые фото котов открываются в отдельных окнах

--- cats2.py
+++ cats2.py
@@ -3,8 +3,6 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-from scipy.special import label
-
 
 def load_image(url):
     try:
@@ -19,11 +17,16 @@ def load_image(url):
         return None
 
 
-def set_image():
+def open_new_window():
     img = load_image(url)
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title("")
+        new_window.geometry("600x480")
+        label = Label(new_window, image=img)
+        label.pack()
         label.image = img
+
 
 
 def exit():
@@ -43,7 +46,7 @@ window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Файл", menu=file_menu)
-file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_command(label="Загрузить фото", command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command=exit)
 
